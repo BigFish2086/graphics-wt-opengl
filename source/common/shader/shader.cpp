2aeb8884d3e0ed34a09683271cc387380d6b5f10
BigFish2086 <a8686.ibrahim@gmail.com>
2022-03-21 15:45:59 +0200

    fill in shader.hpp shader.cpp

--- source/common/shader/shader.cpp
+++ source/common/shader/shader.cpp
@@ -1,68 +1,92 @@
 #include "shader.hpp"
 
 #include <cassert>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 void our::ShaderProgram::create() {
-    //Create Shader Program
+    // Create Shader Program
     program = glCreateProgram();
 }
 
 void our::ShaderProgram::destroy() {
-    //Delete Shader Program
-    if(program != 0) glDeleteProgram(program);
+    // Delete Shader Program
+    if (program != 0)
+        glDeleteProgram(program);
     program = 0;
 }
 
-//Forward definition for error checking functions
+// Forward definition for error checking functions
 std::string checkForShaderCompilationErrors(GLuint shader);
 std::string checkForLinkingErrors(GLuint program);
 
-bool our::ShaderProgram::attach(const std::string &filename, GLenum type) const {
-    // Here, we open the file and read a string from it containing the GLSL code of our shader
+bool our::ShaderProgram::attach(const std::string &filename,
+                                GLenum type) const {
+    // Here, we open the file and read a string from it containing the GLSL code
+    // of our shader
     std::ifstream file(filename);
-    if(!file){
-        std::cerr << "ERROR: Couldn't open shader file: " << filename << std::endl;
+    if (!file) {
+        std::cerr << "ERROR: Couldn't open shader file: " << filename
+                  << std::endl;
         return false;
     }
-    std::string sourceString = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
-    const char* sourceCStr = sourceString.c_str();
+    std::string sourceString = std::string(std::istreambuf_iterator<char>(file),
+                                           std::istreambuf_iterator<char>());
+    const char *sourceCStr = sourceString.c_str();
     file.close();
 
     GLuint shaderID = glCreateShader(type);
 
-    //TODO: send the source code to the shader and compile it
-    
+    // TODO: send the source code to the shader and compile it
+    // void glShaderSource(GLuint shader, GLsizei count, const GLchar **string,
+    //                     const GLint *length);
+    // shader
+    //     Specifies the handle of the shader object whose source code is to
+    //     be replaced.
+    // count
+    //     Specifies the number of elements in the string and length arrays.
+    // string
+    //     Specifies an array of pointers to strings containing the source
+    //     code to be loaded into the shader.
+    // length
+    //     Specifies an array of string lengths. If it's nullptr, each string is
+    //     assumed to be null-terminated
+    //
+    glShaderSource(shaderID, 1, &sourceCStr, nullptr);
+    glCompileShader(shaderID);
+
     // Here we check for compilation errors
-    //TODO: Uncomment this if block
-    // if(std::string error = checkForShaderCompilationErrors(shaderID); error.size() != 0){
-    //     std::cerr << "ERROR IN " << filename << std::endl;
-    //     std::cerr << error << std::endl;
-    //     glDeleteShader(shaderID);
-    //     return false;
-    // }
+    // TODO: Uncomment this if block
+    if (std::string error = checkForShaderCompilationErrors(shaderID);
+        error.size() != 0) {
+        std::cerr << "ERROR IN " << filename << std::endl;
+        std::cerr << error << std::endl;
+        // OpenGL copies the shader source code strings when glShaderSource is
+        // called, so an application may free its copy of the source code
+        // strings immediately after the function returns.
+        glDeleteShader(shaderID);
+        return false;
+    }
 
-    
-    //TODO: attach the shader to the program then delete the shader
+    // TODO: attach the shader to the program then delete the shader
+    glAttachShader(this->program, shaderID);
 
-    //We return true since the compilation succeeded
+    // We return true since the compilation succeeded
     return true;
 }
 
-
-
 bool our::ShaderProgram::link() const {
-    //TODO: call opengl to link the program identified by this->program 
+    // TODO: call opengl to link the program identified by this->program
+    glLinkProgram(this->program);
 
     // Here we check for linking errors
-    //TODO: Uncomment this if block
-    // if(auto error = checkForLinkingErrors(program); error.size() != 0){
-    //     std::cerr << "LINKING ERROR" << std::endl;
-    //     std::cerr << error << std::endl;
-    //     return false;
-    // }
+    // TODO: Uncomment this if block
+    if (auto error = checkForLinkingErrors(program); error.size() != 0) {
+        std::cerr << "LINKING ERROR" << std::endl;
+        std::cerr << error << std::endl;
+        return false;
+    }
 
     return true;
 }
@@ -71,8 +95,8 @@ bool our::ShaderProgram::link() const {
 // Function to check for compilation and linking error in shaders //
 ////////////////////////////////////////////////////////////////////
 
-std::string checkForShaderCompilationErrors(GLuint shader){
-     //Check and return any error in the compilation process
+std::string checkForShaderCompilationErrors(GLuint shader) {
+    // Check and return any error in the compilation process
     GLint status;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
     if (!status) {
@@ -87,8 +111,8 @@ std::string checkForShaderCompilationErrors(GLuint shader){
     return std::string();
 }
 
-std::string checkForLinkingErrors(GLuint program){
-     //Check and return any error in the linking process
+std::string checkForLinkingErrors(GLuint program) {
+    // Check and return any error in the linking process
     GLint status;
     glGetProgramiv(program, GL_LINK_STATUS, &status);
     if (!status) {
--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -1,6 +1,7 @@
 #ifndef SHADER_HPP
 #define SHADER_HPP
 
+#include <new>
 #include <string>
 
 #include <glad/gl.h>
@@ -9,52 +10,60 @@
 
 namespace our {
 
-    class ShaderProgram {
+class ShaderProgram {
 
-    private:
-        //Shader Program Handle
-        GLuint program;
+private:
+    // Shader Program Handle
+    GLuint program;
 
-    public:
-        void create();
-        void destroy();
+public:
+    void create();
+    void destroy();
 
-        ShaderProgram(){ program = 0; }
-        ~ShaderProgram(){ destroy(); }
+    ShaderProgram() { program = 0; }
+    ~ShaderProgram() { destroy(); }
 
-        bool attach(const std::string &filename, GLenum type) const;
+    bool attach(const std::string &filename, GLenum type) const;
 
-        bool link() const;
+    bool link() const;
 
-        void use() { 
-            //TODO: call opengl to use the program identified by this->program
-        }
+    void use() {
+        // TODO: call opengl to use the program identified by this->program
+        glUseProgram(program);
+    }
 
-        GLuint getUniformLocation(const std::string &name) {
-            //TODO: call opengl to get the uniform location for the uniform defined by name from this->program
-        }
+    GLuint getUniformLocation(const std::string &name) {
+        // TODO: call opengl to get the uniform location for the uniform defined
+        // by name from this->program
+        return glGetUniformLocation(this->program, name.c_str());
+    }
 
-        void set(const std::string &uniform, GLfloat value) {
-            //TODO: call opengl to set the value to the uniform defined by name
-        }
+    void set(const std::string &uniform, GLfloat value) {
+        // TODO: call opengl to set the value to the uniform defined by name
+        glUniform1f(this->getUniformLocation(uniform), value);
+    }
 
-        void set(const std::string &uniform, glm::vec2 value) {
-            //TODO: call opengl to set the value to the uniform defined by name
-        }
+    void set(const std::string &uniform, glm::vec2 value) {
+        // TODO: call opengl to set the value to the uniform defined by name
+        glUniform2fv(this->getUniformLocation(uniform), 1, &value[0]);
+    }
 
-        void set(const std::string &uniform, glm::vec3 value) {
-            //TODO: call opengl to set the value to the uniform defined by name
-        }
+    void set(const std::string &uniform, glm::vec3 value) {
+        // TODO: call opengl to set the value to the uniform defined by name
+        glUniform3fv(this->getUniformLocation(uniform), 1, &value[0]);
+    }
 
-        void set(const std::string &uniform, glm::vec4 value) {
-            //TODO: call opengl to set the value to the uniform defined by name
-        }
+    void set(const std::string &uniform, glm::vec4 value) {
+        // TODO: call opengl to set the value to the uniform defined by name
+        glUniform4fv(this->getUniformLocation(uniform), 1, &value[0]);
+    }
 
+    // TODO: Delete the copy constructor and assignment operator
+    // Question: Why do we do this? Hint: Look at the deconstructor
+    ShaderProgram(const ShaderProgram &other) = delete;
+    ShaderProgram operator==(const ShaderProgram &other) = delete;
+};
 
-        //TODO: Delete the copy constructor and assignment operator
-        //Question: Why do we do this? Hint: Look at the deconstructor
-    };
-
-}
+} // namespace our
 
 #endif
