41b9edcb7882ef84f1cfa516c331f945afdeac16
BigFish2086 <a8686.ibrahim@gmail.com>
2022-04-23 11:22:33 +0200

    Complete texture requirement

--- source/common/texture/texture2d.hpp
+++ source/common/texture/texture2d.hpp
@@ -4,42 +4,42 @@
 
 namespace our {
 
-    // This class defined an OpenGL texture which will be used as a GL_TEXTURE_2D
-    class Texture2D {
-        // The OpenGL object name of this texture 
-        GLuint name = 0;
-    public:
-        // This constructor creates an OpenGL texture and saves its object name in the member variable "name" 
-        Texture2D() {
-            //TODO: (Req 4) Complete this function
-            
-        };
+// This class defined an OpenGL texture which will be used as a GL_TEXTURE_2D
+class Texture2D {
+    // The OpenGL object name of this texture
+    GLuint name = 0;
 
-        // This deconstructor deletes the underlying OpenGL texture
-        ~Texture2D() { 
-            //TODO: (Req 4) Complete this function
-
-        }
-
-        // Get the internal OpenGL name of the texture which is useful for use with framebuffers
-        GLuint getOpenGLName() {
-            return name;
-        }
-
-        // This method binds this texture to GL_TEXTURE_2D
-        void bind() const {
-            //TODO: (Req 4) Complete this function
-
-        }
-
-        // This static method ensures that no texture is bound to GL_TEXTURE_2D
-        static void unbind(){
-            //TODO: (Req 4) Complete this function
-
-        }
-
-        Texture2D(const Texture2D&) = delete;
-        Texture2D& operator=(const Texture2D&) = delete;
+public:
+    // This constructor creates an OpenGL texture and saves its object name in the member variable "name"
+    Texture2D() {
+        // TODO: (Req 4) Complete this function
+        glGenTextures(1, &name); // generate single texture
     };
-    
-}
+
+    // This deconstructor deletes the underlying OpenGL texture
+    ~Texture2D() {
+        // TODO: (Req 4) Complete this function
+        glDeleteTextures(1, &name);
+    }
+
+    // Get the internal OpenGL name of the texture which is useful for use with framebuffers
+    GLuint getOpenGLName() { return name; }
+
+    // This method binds this texture to GL_TEXTURE_2D
+    void bind() const {
+        // TODO: (Req 4) Complete this function
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, name); // To work with our texture as a 2D texture, we bind it to GL_TEXTURE_2D.
+    }
+
+    // This static method ensures that no texture is bound to GL_TEXTURE_2D
+    static void unbind() {
+        // TODO: (Req 4) Complete this function
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    Texture2D(const Texture2D &) = delete;
+    Texture2D &operator=(const Texture2D &) = delete;
+};
+
+} // namespace our
